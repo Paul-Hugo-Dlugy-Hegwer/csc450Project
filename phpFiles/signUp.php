7bfea367e6b705f83aa907635fcc1f616ee0905a
kpage1 <48829396+kpage1@users.noreply.github.com>
2020-05-07 13:52:42 -0400

    Update signUp.php

--- phpFiles/signUp.php
+++ phpFiles/signUp.php
@@ -1,3 +1,18 @@
+/*
+ * Title: signUp.php
+ * Purpose: The page responsible for signing up users or organizations for an account.
+ * Sample Call (or Use): include 'connection.php';
+ * Inputs (or Imports): Takes information from user to create a username and password for the account. 
+ * Outputs (or Exports): When the creation is successful, the username, password, email, phone, address are entered into the database.
+ * Subroutines Used (or Defined): session_start, OpenCon(), mysqli
+ *--------------------SQL Queries
+ *--------------------INSERT INTO user(username, password, email, phone, address) VALUES ('$un_var', '$pw_var', '$e_var', '$pn_var', '$ad_var');
+ *--------------------INSERT INTO organization(username, password, email, phone, address) VALUES ('$un_var', '$pw_var', '$e_var', '$pn_var', '$ad_var');
+ * Author: Paul-Hugo Dlugy-Hegwer
+ * Date: Unknown
+ * Modifications (when & what): Modifications made on 05/07/2020.
+ * */
+
 <!DOCTYPE html>
 <?php
 session_start();
