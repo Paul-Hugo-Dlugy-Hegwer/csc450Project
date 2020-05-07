41ca2bdb24145d4d6da74cf1f130ebd48cd1eb07
Dlugy-hegwer <pd4235@satoshi.cis.uncw.edu>
2020-05-06 23:54:54 -0400

    Adds new non survey monkey surveys

--- phpFiles/newPostPage.php
+++ phpFiles/newPostPage.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<?php
-session_start();
-echo $_SESSION['loginType'];
-?>
 <html lang="en">
 <head>
   <title>VolunteerOps</title>
@@ -12,48 +8,43 @@ echo $_SESSION['loginType'];
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    /* Remove the navbar's default margin-bottom and rounded borders */
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
     }
-    
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
     .row.content {height: 450px}
-    
     /* Set gray background color and 100% height */
     .sidenav {
       padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
-    
     /* Set black background color, white text and some padding */
     footer {
       background-color: #555;
       color: white;
       padding: 15px;
     }
-    
     /* On small screens, set height to 'auto' for sidenav and grid */
     @media screen and (max-width: 767px) {
       .sidenav {
         height: auto;
         padding: 15px;
       }
-      .row.content {height:auto;} 
+      .row.content {height:auto;}
     }
   </style>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#">VolunteerOps</a>
     </div>
@@ -63,41 +54,48 @@ echo $_SESSION['loginType'];
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
     </div>
   </div>
 </nav>
-  
-<div class="container-fluid text-center">    
+<div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-2 sidenav">
     </div>
-    <div class="col-sm-8 text-left"> 
-      <h1>Welcome</h1>
-      <p>Welcome to Volunteer Ops, to find opportunities to give back to your
-      community please create an account and login.</p>
-      <?php if ($_SESSION['check']  == 'Submit' && $_SESSION['loginType']=='user') : ?>
-      <button type="button" onclick="window.location.href='newSurveyPage.php'">Take Survey</button>
-      
-      <button type="button" onclick="alert('VO')">View opportinities</button>
-      <?php endif ?>
-      <?php if ($_SESSION['loginType']=='org') : ?>
-	<button type="button" onclick="window.location.href='newPostPage.php'">Post Opportunity</button>
+    <div class="col-sm-8 text-left">
+      <h1>To setup a volunteering opportunity, fill out the details form.</h1>
+      <form method="POST" action="login.php">
+              <h2>Name of Opportunity </h2>
+              <input name = "opName"/>
+              <h3>description</h3>
+              <input name = "descript"/>
+              <h3>Physical Activity</h3>
+              True <input type="Radio" name="phyAc" value="1"/>
+	      False <input type="Radio" name="phyAc" value="0"/>
+              <h3>Tech Skills Required</h3>
+              True <input type="Radio" name="techSkls" value="1"/>
+	      False <input type="Radio" name="techSkls" value="0"/>
+              <h3>Social Activity</h3>
+	      True <input type="Radio" name="socAc" value="1"/>
 
-      	<button type="button" onclick="alert('VO')">View applicants</button>
-      <?php endif ?>
-      <hr>
+	      False <input type="Radio" name="socAc" value="0"/>
+              <h3>Zipcode</h3>
+	      <input name = "zipCode"/>
+	      <h3>City</h3>
+	      <input name = "city"/>
+	      <h3>State</h3>
+	      <input name = "state"/>
+	      <h3>Time</h3>
+	      <input name = "time"/>
+	            <P>
+              <input type="submit" name="submit" value="Submit"/>
+      </form>
     </div>
     <div class="col-sm-2 sidenav">
     </div>
   </div>
 </div>
-
 <footer class="container-fluid text-center">
   <p>Footer Text</p>
 </footer>
-
 </body>
 </html>
